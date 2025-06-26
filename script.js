0f416ef5470a5b902a05a5cedbd06baed0a79e78
Kris <kris.shcheglova@bk.ru>
2025-06-26 13:52:49 +0300

    change mobile style

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const swiper = new Swiper('.reviews', {
     direction: 'horizontal',
-    slidesPerView: 3,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
     },
 });
 
